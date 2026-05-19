8e02080ee47bcfbbb3d7b39e6e5f6af45d7bad55
Om Patel <omptl213@gmail.com>
2026-05-19 14:29:33 +0530

    Enhance Half Adder testbench with $monitor
    Added $monitor statement to display simulation results.

--- Combinational Circuits/Half Adder/Half_Adder_tb.v
+++ Combinational Circuits/Half Adder/Half_Adder_tb.v
@@ -5,18 +5,17 @@ module Half_Adder_tb;
 reg a, b;
 wire s, cout;
 
-Half_Adder dut (
-    .a(a),
-    .b(b),
-    .s(s),
-    .cout(cout)
-);
+Half_Adder dut(.a(a),.b(b),.s(s),.cout(cout));
 
-initial begin
+initial 
+begin
+    $monitor("Time=%0t a=%b b=%b sum=%b cout=%b", $time, a, b, s, cout);
+
     a = 0; b = 0; #10;
     a = 0; b = 1; #10;
     a = 1; b = 0; #10;
     a = 1; b = 1; #10;
+
     $finish;
 end
 
